2d89ec6f3dd8f9ee3fbf9193333ae22a74d08312
alex.filatov <alex.filatov@sparox.ru>
2021-04-03 14:15:50 +0300

    create job4j_cars

---
+++ src/main/java/ru/af3412/job4j_cars/model/car/Model.java
@@ -0,0 +1,28 @@
+package ru.af3412.job4j_cars.model.car;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "models")
+public class Model {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
